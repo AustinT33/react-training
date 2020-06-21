58e4a676c5e59f5c8b7a42d5d2b6b490f0a4cf6f
austint33 <austintumlinson@gmail.com>
2020-06-21 18:32:41 -0500

    incorporating thunk, solved prior build issues

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -3,15 +3,16 @@ import { connect } from 'react-redux';
 import TodoListItem from '../TodoListItem/TodoListItem';
 import NewTodoForm from '../NewTodoForm/NewTodoForm';
 import { removeTodo, markTodoAsCompleted } from '../../Redux/actions';
+import { displayAlert } from '../../Redux/thunks';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, onDisplayAlertClicked }) => (
     <div className="list-wrapper">
     <NewTodoForm />
     {todos.map(todo => <TodoListItem 
         todo={todo} 
         onRemovePressed={onRemovePressed} 
-        onCompletedPressed={onCompletedPressed} />)}
+        onCompletedPressed={onDisplayAlertClicked} />)}
     </div>
 );
 
@@ -21,6 +22,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     onRemovePressed: text => dispatch(removeTodo(text)),
     onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
+    onDisplayAlertClicked: text => dispatch(displayAlert(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
