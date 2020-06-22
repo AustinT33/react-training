d66493317b160f71ef2e8cd199401d99a4a2e849
austint33 <austintumlinson@gmail.com>
2020-06-22 13:13:06 -0500

    implemented api fetch and subsequent actions for thunk/redux

--- src/Redux/thunks.js
+++ src/Redux/thunks.js
@@ -1,3 +1,18 @@
+import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from './actions';
+
+export const loadTools = () => async (dispatch, getState) => {
+    try {
+        dispatch(loadTodosInProgress());
+        const res = await fetch('http://localhost:8080/todos');
+        const todos = await res.json();
+
+        dispatch(loadTodosSuccess(todos));
+    } catch (e) {
+        dispatch(loadTodosFailure());
+        dispatch(displayAlert(e));
+    }
+}
+
 export const displayAlert = text => () => {
-    alert(`you clicked on: ${text}`);
+    alert(text);
 }
